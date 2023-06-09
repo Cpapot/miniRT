0209455d1cf453d92f4aed2e68167e038db921bd
bpoumeau <delafforest.brieuc@gmail.com>
2023-06-09 13:25:35 +0200

    headers files

---
+++ srcs/parsing/parsing.h
@@ -0,0 +1,8 @@
+//
+// Created by bpoumeau on 6/9/23.
+//
+
+#ifndef RAYTRACER_PARSING_H
+#define RAYTRACER_PARSING_H
+
+#endif //RAYTRACER_PARSING_H
---
+++ srcs/parsing/structures.h
@@ -0,0 +1,8 @@
+//
+// Created by bpoumeau on 6/9/23.
+//
+
+#ifndef RAYTRACER_STRUCTURES_H
+#define RAYTRACER_STRUCTURES_H
+
+#endif //RAYTRACER_STRUCTURES_H
