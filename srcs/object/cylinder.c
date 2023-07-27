47d48ef6217c970855927dbf20860ad120f2d277
coromoEXE <corentinpapot2004@gmail.com>
2023-07-27 18:32:31 +0200

    cone + cylinder ok (without disk)

--- srcs/object/cylinder.c
+++ srcs/object/cylinder.c
@@ -6,18 +6,20 @@
 /*   By: cpapot <cpapot@student.42lyon.fr >         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/23 15:54:31 by cpapot            #+#    #+#             */
-/*   Updated: 2023/07/23 16:26:23 by cpapot           ###   ########.fr       */
+/*   Updated: 2023/07/27 18:26:59 by cpapot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 #include "vec3.h"
 
-t_vec_3 cylinder_normal(t_ray camray)
+t_vec_3 cylinder_normal(t_ray camray, double t)
 {
 	t_vec_3	result;
+	t_point	dot;
 
-	result = set_vec(camray.origin.x, 0,camray.origin.z);
+	dot = hit_coord(t, camray);
+	result = set_vec(dot.x, 0, dot.z);
 	normalize_vec(&result);
 	return (result);
 }
@@ -29,17 +31,41 @@ double	cylinder_hitted(t_ray camray, t_cylinder cyl)
 	double	C;
 	double	delta;
 	double	t;
+	t_point	hitpoint;
+	t_vec_3	cyl_vect[3];
+	t_vec_3	tmp;
+	t_vec_3	vec;
 
-	A = cyl.normal_vector.x * cyl.normal_vector.x + cyl.normal_vector.z * cyl.normal_vector.z;
-	B = 2 * (cyl.normal_vector.x * camray.origin.x + cyl.normal_vector.z * camray.origin.z);
-	C = pow((camray.direction.x - cyl.coordinate.x), 2) + pow((camray.direction.z - cyl.coordinate.z), 2) - (pow(cyl.diameter / 2, 2));
+	normalize_vec(&cyl.normal_vector);
+
+	cyl_vect[0] = set_vec(camray.origin.x - cyl.coordinate.x, camray.origin.y - cyl.coordinate.y, camray.origin.z - cyl.coordinate.z);
+
+	tmp = cyl.normal_vector;
+	multiplying_vec(&tmp, scalar_product(camray.direction, cyl.normal_vector));
+	cyl_vect[1] = set_vec(camray.direction.x - tmp.x, camray.direction.y - tmp.y, camray.direction.z - tmp.z);
+
+	tmp = cyl.normal_vector;
+	multiplying_vec(&tmp, scalar_product(cyl_vect[0], cyl.normal_vector));
+	cyl_vect[2] = set_vec(cyl_vect[0].x - tmp.x, cyl_vect[0].y - tmp.y, cyl_vect[0].z - tmp.z);
+
+	A = scalar_product(cyl_vect[1], cyl_vect[1]);
+	if (A <= 0)
+		return (-1);
+	B = 2 * scalar_product(cyl_vect[1], cyl_vect[2]);
+	C = scalar_product(cyl_vect[2], cyl_vect[2]) - pow(cyl.diameter / 2, 2);
 	delta = B * B - 4 * A * C;
 	if (delta < 0)
 		return (-1);
 	delta = sqrtf(delta);
-	t = ((-B - delta)/(2*A));
-	if (t > (-B + delta)/(2*A) && (-B + delta)/(2*A) > 0)
-		t = (-B + delta)/(2*A);
+	t = (-B - delta) / (2.0f * A);
+	if (t < 0 || (((-B + delta) / (2.0f * A)) > 0 && ((-B + delta) / (2.0f * A)) < t))
+		t = (-B + delta) / (2.0f * A);
+	if (t < 0)
+		return (-1);
+	hitpoint = hit_coord(t, camray);
+	vec = set_vec(hitpoint.x, hitpoint.y, hitpoint.z);
+	if (scalar_product(vec, cyl.normal_vector) < 0 || scalar_product(vec, cyl.normal_vector) > cyl.height)
+		return (-1);
 	return (t);
 }
 
@@ -73,7 +99,7 @@ int32_t	render_cylinder(t_hitinfo info, t_ray camray, t_minirt_data data)
 
 	cy = (t_cylinder *)info.struct_info;
 	ratio = ft_find_light_ratio(hit_coord(info.t, camray), data, \
-	cylinder_normal(camray));
+	cylinder_normal(camray, info.t));
 	ambient_lightning(&ratio, &data);
 	return (ft_color(cy->color.r * ratio.r, cy->color.g * \
 	ratio.g, cy->color.b * ratio.b, 0));
