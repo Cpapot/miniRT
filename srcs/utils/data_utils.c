e343cf87812721cbb531eb1e86a8bdd17cf8bb4a
bpoumeau <delafforest.brieuc@gmail.com>
2023-09-06 14:05:46 +0200

    delete add disk

--- srcs/utils/data_utils.c
+++ srcs/utils/data_utils.c
@@ -42,43 +42,6 @@ t_disk	*copy_old_diskdata(t_minirt_data *data_pt)
 	return (disk_arr);
 }
 
-bool	add_disk(t_minirt_data *data_pt)
-{
-	size_t	index;
-	size_t	disk_index;
-	t_disk	*disk_arr;
-	t_vec_3	cyl_vec;
-
-	index = 0;
-	disk_index = 0;
-	disk_arr = copy_old_diskdata(data_pt);
-	if (!disk_arr)
-		return (false);
-	while (data_pt->cy_nb != index)
-	{
-		disk_arr[data_pt->di_nb + disk_index].color = data_pt->cylinder_arr[index].color;
-		disk_arr[data_pt->di_nb + disk_index].normal_vector = data_pt->cylinder_arr[index].normal_vector;
-		disk_arr[data_pt->di_nb + disk_index].diameter = data_pt->cylinder_arr[index].diameter;
-		disk_arr[data_pt->di_nb + disk_index].coordinate = data_pt->cylinder_arr[index].coordinate;
-		disk_index++;
-		disk_arr[data_pt->di_nb + disk_index].color = data_pt->cylinder_arr[index].color;
-		disk_arr[data_pt->di_nb + disk_index].normal_vector = data_pt->cylinder_arr[index].normal_vector;
-		disk_arr[data_pt->di_nb + disk_index].diameter = data_pt->cylinder_arr[index].diameter;
-		cyl_vec = data_pt->cylinder_arr[index].normal_vector;
-		multiplying_vec(&cyl_vec, -(data_pt->cylinder_arr[index].height));
-		disk_arr[data_pt->di_nb + disk_index].coordinate.x = cyl_vec.x + disk_arr[data_pt->di_nb + disk_index - 1].coordinate.x;
-		disk_arr[data_pt->di_nb + disk_index].coordinate.y = cyl_vec.y + disk_arr[data_pt->di_nb + disk_index - 1].coordinate.y;
-		disk_arr[data_pt->di_nb + disk_index].coordinate.z = cyl_vec.z + disk_arr[data_pt->di_nb + disk_index - 1].coordinate.z;
-		disk_arr[data_pt->di_nb + disk_index].normal_vector = data_pt->cylinder_arr[index].normal_vector;
-		multiplying_vec(&disk_arr[data_pt->di_nb + disk_index].normal_vector, -1);
-		disk_index++;
-		index++;
-	}
-	data_pt->di_nb = data_pt->di_nb + data_pt->cy_nb * 2;
-	data_pt->disk_arr = disk_arr;
-	return (true);
-}
-
 void	*suppress_light(t_light light, t_minirt_data *data_pt)
 {
 	size_t	lt_nb;
